ab3707b86f1d3891ffe02f8633cf31fb57c6c059
Samuel Hempelt <146424612+hempelt@users.noreply.github.com>
2025-06-20 13:37:10 +0200

    add opmimized parameter

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,8 +1,6 @@
 import pandas as pd
-import numpy as np
 import joblib
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
@@ -14,20 +12,9 @@ df.head() #Display the first 5 rows of the DataFrame
 X = df.drop(columns=['tm_c'])  # Drop the target variable 'tm_c' from the features
 y = df['tm_c']   
 
-# --- Daten aufteilen
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Train gradient boosting regressor model with optimized hyperparamter
+model = GradientBoostingRegressor(learning_rate= 0.1, max_depth= 4, n_estimators= 200, subsample= 0.8, random_state=42)
+model.fit(X, y)
 
-# --- Feature Scaling
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
-
-# --- Modell trainieren
-model = GradientBoostingRegressor(random_state=42)
-model.fit(X_train_scaled, y_train)
-
-# Modell speichern
+# Save model
 joblib.dump(model, 'C:/Users/hempe/Studium/Real_Project/Project_repo/models/gradient_boosting_model.pkl')
-
-# Optional: auch den Scaler speichern
-joblib.dump(scaler, 'C:/Users/hempe/Studium/Real_Project/Project_repo/models/modelsscaler.pkl')
