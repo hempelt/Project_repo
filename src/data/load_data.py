672a0a5237b9f30ab628879124d6a4f2cc0c8ff7
Samuel Hempelt <146424612+hempelt@users.noreply.github.com>
2025-07-11 17:12:06 +0200

    build in simple pydantic data validation after loading the data from database

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -3,6 +3,7 @@ import pandas as pd
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
+from pydantic import BaseModel, TypeAdapter, ValidationError
 
 # Load database connection parameters from .env file
 load_dotenv()
@@ -36,10 +37,34 @@ except SQLAlchemyError as e:
 finally:
     engine.dispose()
 
-# Export DataFrame to CSV
+# Validate datatypes of important variables in dataframe with pydantic
+# Define a Pydantic model for the measurement data
+class Measurement(BaseModel):
+    isoelectric_point: float
+    protein_format: str
+    molecular_weight_da: float
+    composition: str
+    product_conc_mg_ml: float
+    tm_c: float
+
+# Create a TypeAdapter for the Measurement model
+adapter = TypeAdapter(list[Measurement])
+
+
 if df is not None:
-    output_csv_path = 'data/raw/raw_data.csv'
-    df.to_csv(output_csv_path, index=False) 
-    print(f"✅ Data saved as CSV file to {output_csv_path}")
+    # Validate the DataFrame against the Pydantic model
+    # Convert DataFrame to a list of dictionaries for validation
+    try:
+        validated = adapter.validate_python(df.to_dict(orient="records"))
+        print("✅ No data type validation errors found.")
+        # Export the validated DataFrame to a CSV file
+        output_csv_path = 'data/raw/raw_data.csv'
+        df.to_csv(output_csv_path, index=False) 
+        print(f"✅ Data saved as CSV file to {output_csv_path}")
+    except ValidationError as e:
+        print("❌ Validation failed for one or more rows:")
+        print("Error:", e)
+        print("❌ Data was not exported due to validation errors.")
 else:
     print("❌ No data to save, DataFrame is None.")
+    
